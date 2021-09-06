1995b1a2751c1fe365415cb1d0e7f1009a875457
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-09-06 23:50:33 +0300

    add react boilerplate/template to github

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import ReactDom from 'react-dom';
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return <h2>godfreyowiidi@gmail.com</h2>;
+}
+
+ReactDom.render (<App />, document.getElementById('app'));
